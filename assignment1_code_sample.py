6d0bee1a0f27d5fb65afd4e4cc7b68fd8e6d8cbd
Noah Manaigre <nmanaigre2@academic.rrc.ca>
2025-03-17 00:32:37 -0500

    Made a change for Appscan trigger.

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -8,9 +8,12 @@ db_config = {
     'password': 'secret123'
 }
 
+    
+
 def get_user_input():
     user_input = input('Enter your name: ')
     return user_input
+    
 
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
@@ -21,7 +24,8 @@ def get_data():
     return data
 
 def save_to_db(data):
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    query = "INSERT INTO mytable (column1, column2) VALUES ('" + data + "', 'Another Value')"
+
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
     cursor.execute(query)
@@ -35,3 +39,4 @@ if __name__ == '__main__':
     save_to_db(data)
     send_email('admin@example.com', 'User Input', user_input)
     
+
